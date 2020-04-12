2cbedbed73994a1ba0cf86eaa8d1c21f3a21c1ea
Ibrahim Adedokun <N0828199@my.ntu.ac.uk>
2020-04-13 00:19:03 +0100

    Add project files.

---
+++ IslamicAndArabic/IslamicAndArabic/DropDownPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using IslamicAndArabic.MarkupExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IslamicAndArabic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DropDownPage : ContentPage
+    {
+        public DropDownPage()
+        {
+            InitializeComponent();
+        }
+
+        private void firstButton_Clicked(object sender, EventArgs e)
+        {
+            if(firstStackOne.IsVisible == true && firstStackTwo.IsVisible == true && firstStackThree.IsVisible == true)
+            {
+                firstButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.plus64px.png");
+                firstStackOne.IsVisible = false;
+                firstStackTwo.IsVisible = false;
+                firstStackThree.IsVisible = false;
+            }
+            else if (firstStackOne.IsVisible == false && firstStackTwo.IsVisible == false && firstStackThree.IsVisible == false)
+            {
+                firstButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.minus64px.png");
+                firstStackOne.IsVisible = true;
+                firstStackTwo.IsVisible = true;
+                firstStackThree.IsVisible = true;
+            }
+
+        }
+    }
+}
