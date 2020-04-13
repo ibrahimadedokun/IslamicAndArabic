f374de4b7ab175e99bcdce5015ae5f60d7fe361b
Ibrahim Adedokun <N0828199@my.ntu.ac.uk>
2020-04-13 17:21:08 +0100

    sub contetnts clickable

--- IslamicAndArabic/IslamicAndArabic/DropDownPage.xaml.cs
+++ IslamicAndArabic/IslamicAndArabic/DropDownPage.xaml.cs
@@ -34,7 +34,103 @@ namespace IslamicAndArabic
                 firstStackTwo.IsVisible = true;
                 firstStackThree.IsVisible = true;
             }
+        }
 
+        private void secondButton_Clicked(object sender, EventArgs e)
+        {
+            if (secondStackOne.IsVisible == true && secondStackTwo.IsVisible == true && secondStackThree.IsVisible == true && secondStackFour.IsVisible == true)
+            {
+                secondButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.plus64px.png");
+                secondStackOne.IsVisible = false;
+                secondStackTwo.IsVisible = false;
+                secondStackThree.IsVisible = false;
+                secondStackFour.IsVisible = false;
+            }
+            else if (secondStackOne.IsVisible == false && secondStackTwo.IsVisible == false && secondStackThree.IsVisible == false && secondStackFour.IsVisible == false)
+            {
+                secondButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.minus64px.png");
+                secondStackOne.IsVisible = true;
+                secondStackTwo.IsVisible = true;
+                secondStackThree.IsVisible = true;
+                secondStackFour.IsVisible = true;
+            }
+        }
+
+        private void thirdButton_Clicked(object sender, EventArgs e)
+        {
+            if (thirdStackOne.IsVisible == true && thirdStackTwo.IsVisible == true && thirdStackThree.IsVisible == true)
+            {
+                thirdButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.plus64px.png");
+                thirdStackOne.IsVisible = false;
+                thirdStackTwo.IsVisible = false;
+                thirdStackThree.IsVisible = false;
+            }
+            else if (thirdStackOne.IsVisible == false && thirdStackTwo.IsVisible == false && thirdStackThree.IsVisible == false)
+            {
+                thirdButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.minus64px.png");
+                thirdStackOne.IsVisible = true;
+                thirdStackTwo.IsVisible = true;
+                thirdStackThree.IsVisible = true;
+            }
+        }
+
+        private void fourthButton_Clicked(object sender, EventArgs e)
+        {
+            if (fourthStackOne.IsVisible == true)
+            {
+                fourthButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.plus64px.png");
+                fourthStackOne.IsVisible = false;
+            }
+            else if (fourthStackOne.IsVisible == false)
+            {
+                fourthButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.minus64px.png");
+                fourthStackOne.IsVisible = true;
+            }
+        }
+
+        private void fifthButton_Clicked(object sender, EventArgs e)
+        {
+            if (fifthStackOne.IsVisible == true && fifthStackTwo.IsVisible == true)
+            {
+                fifthButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.plus64px.png");
+                fifthStackOne.IsVisible = false;
+                fifthStackTwo.IsVisible = false;
+            }
+            else if (fifthStackOne.IsVisible == false && fifthStackTwo.IsVisible == false)
+            {
+                fifthButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.minus64px.png");
+                fifthStackOne.IsVisible = true;
+                fifthStackTwo.IsVisible = true;
+            }
+        }
+
+        private void sixthButton_Clicked(object sender, EventArgs e)
+        {
+            if (sixthStackOne.IsVisible == true && sixthStackTwo.IsVisible == true && sixthStackThree.IsVisible == true)
+            {
+                sixthButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.plus64px.png");
+                sixthStackOne.IsVisible = false;
+                sixthStackTwo.IsVisible = false;
+                sixthStackThree.IsVisible = false;
+                sixthStackFour.IsVisible = false;
+                sixthStackFive.IsVisible = false;
+                sixthStackSix.IsVisible = false;
+            }
+            else if (sixthStackOne.IsVisible == false && sixthStackTwo.IsVisible == false && sixthStackThree.IsVisible == false)
+            {
+                sixthButton.ImageSource = ImageSource.FromResource("IslamicAndArabic.images.minus64px.png");
+                sixthStackOne.IsVisible = true;
+                sixthStackTwo.IsVisible = true;
+                sixthStackThree.IsVisible = true;
+                sixthStackFour.IsVisible = true;
+                sixthStackFive.IsVisible = true;
+                sixthStackSix.IsVisible = true;
+            }
+        }
+
+        async private void fourthOne_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page());
         }
     }
 }
